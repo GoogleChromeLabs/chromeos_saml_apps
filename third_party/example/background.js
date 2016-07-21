db896029eb031cc1c35c887c11632cf312b3cc6f
David Karam <dskaram@google.com>
2016-07-21 12:41:54 +0200

    Fix example code to show proper licenses

--- third_party/example/background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('webview.html', {
-  	id: "webview",
-    innerBounds: {
-      'width': 1024,
-      'height': 768
-    }
-  });
-});
